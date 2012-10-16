3335e2a1299ce63a6490801a5262a9753382c8ec
xintongzhang <annabelle.cmu@gmail.com>
2012-10-16 11:11:16 -0400

    fix_current_user_problem

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,7 +34,8 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+    @current_user = user
+    #self.current_user = user
   end
 
 end
