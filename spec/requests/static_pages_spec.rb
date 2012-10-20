5c90e51d4d4a60cf51b321a9da716e47babc3746
zhou <zhou.wei@sv.cmu.edu>
2012-10-20 04:41:59 -0400

    Finish filling layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,26 @@
-require 'spec_helper'
-
 describe "Static pages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the content 'BESTBAY'" do
-      visit 'static_pages/home'
-      page.should have_content('BESTBAY')
-    end
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'BESTBAY') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 end
