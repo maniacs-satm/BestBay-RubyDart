a45c8f104a27148d8c80345ecac69df27472e18f
ylang <ylang@andrew.cmu.edu>
2012-10-31 04:04:57 -0400

    cnt'd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,7 @@ BestBay::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = false
-  config.action_controller.perform_caching = false
-  Rails.cache.clear
+  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
