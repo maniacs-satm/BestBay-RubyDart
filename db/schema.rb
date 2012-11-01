184c06b939682840a2e3c8d197ad8be32155373b
zhou <zhou.wei@sv.cmu.edu>
2012-11-01 13:38:21 -0400

    revise button display

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,15 @@ ActiveRecord::Schema.define(:version => 20121031154938) do
     t.integer "user_id"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'array' for column 'selling_products'
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+    t.string   "remember_token"
+  end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
