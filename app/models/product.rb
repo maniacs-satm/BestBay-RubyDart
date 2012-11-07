171e657e5676d38499f4a9e2fcfd48a9e86eb155
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-11-07 15:03:06 -0500

    organize product.rb style

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,26 @@
 class Product < ActiveRecord::Base
-  attr_accessible :current_price, :description, :user_id, :start_price, :time_left, :title, :image, :user_name, :category_id
-  has_attached_file :image, :styles => {
-      :thumb=> "250x300#",
-      :small  => "600x600>" }
+  attr_accessible :current_price, 
+                  :description, 
+                  :user_id, 
+                  :user_name,  
+                  :start_price, 
+                  :time_left, 
+                  :title, 
+                  :image, 
+                  :category_id
 
+  has_attached_file :image, :styles => {
+                              :thumb=> "250x300#",
+                              :small  => "600x600>" }
 
   belongs_to :user
   belongs_to :category
   has_many   :bids
   has_many   :watchlists
 
-  validates :title, presence: true, length: { maximum: 200,
-                                              too_long: "title must have at most %{count} characters"}
+  validates :title, presence: true, 
+            length: { maximum: 200,
+                      too_long: "title must have at most %{count} characters"}
   validates :start_price, presence: true
   validates :category_id, presence:true
 end
