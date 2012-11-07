ebd3c1cd4312caa4ade0bc4606e38ca368db02a0
ylang <ylang@andrew.cmu.edu>
2012-11-07 17:45:25 -0500

    precompile

--- db/schema.rb
+++ db/schema.rb
@@ -46,16 +46,13 @@ ActiveRecord::Schema.define(:version => 20121103145955) do
     t.integer  "category_id"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
-    t.string   "remember_token"
+  create_table "products_users", :id => false, :force => true do |t|
+    t.integer "product_id"
+    t.integer "user_id"
   end
 
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'array' for column 'selling_products'
 
   create_table "watchlists", :force => true do |t|
     t.integer  "product_id"
