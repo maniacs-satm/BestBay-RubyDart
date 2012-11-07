98f8891a2ce59f59bd1cc3c49704af9eb7d0e4f0
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-11-07 18:20:25 -0500

    rdoc demo

--- doc/js/search_index.js
+++ doc/js/search_index.js
@@ -1,1 +1,1 @@
-var search_data = {"index":{"searchIndex":["product"],"longSearchIndex":["product"],"info":[["Product","","Product.html","","<p>Schema Information\n<p>Table name: products\n\n<pre>id                 :integer          not null, primary key\ntitle ...</pre>\n"]]}}
+var search_data = {"index":{"searchIndex":["bid"],"longSearchIndex":["bid"],"info":[["Bid","","Bid.html","","<p>Rdoc for Bid table\n<p>Bidding\n<p>This is a heading\n"]]}}
