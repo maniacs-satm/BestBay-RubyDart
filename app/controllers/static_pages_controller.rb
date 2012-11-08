727deb0e84f25d2a4341fec5d1b7673bbeb5cbe3
zhou <zhou.wei@sv.cmu.edu>
2012-11-08 02:35:05 -0500

    Finish-rdoc-staticpages-Home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
+##
+# This is the StaticPages controller that define three methods-home, help and about
+#
 class StaticPagesController < ApplicationController
-
+# This methods will display the homepage view
+# *Renders*
+#   - redirect to product list page -> if user has signed in already
+#   - redirect to home page -> if user has not singed in
+#
   def home   
 	if signed_in?
 		redirect_to controller: 'products', action: 'index'
