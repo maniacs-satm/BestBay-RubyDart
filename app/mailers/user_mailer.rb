07ac2f11b3f3d55a00224042d2e6dfbefb3697b1
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-11-08 14:21:12 -0500

    update user_mailer.rb comments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,23 @@
+# UserMailer is used to define all the email service for BESTBAY.
+#
 class UserMailer < ActionMailer::Base
   default from: "rubydart.bestbay@gmail.com"
 
+  # This method defines the email sent to user when the user successfully.
+  # registers with the system.
+  # * *View* :
+  #   - views/user_mailer/registration_confirmation.text.erb
+  #
   def registration_confirmation(user)
     @user = user
     mail(:to => user.email, :subject => "Registered")
   end
 
+  # This method defines the email sent to user when the user successfully.
+  # bids a product.
+  # * *View* :
+  #   - views/user_mailer/bid_success_confirmation.text.erb
+  #
   def bid_success_confirmation(user, product)
     @product = product
     @user = user
