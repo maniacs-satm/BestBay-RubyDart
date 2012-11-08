8c7c81c5d0bd853293c26e3094f3d69f40da339f
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-11-08 11:09:58 -0500

    update test for model bid.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,12 @@ FactoryGirl.define do
     password              "foobar"
     password_confirmation "foobar"
   end
+
+  # Bid model
+  factory :bid do
+    bidding_time   "2012-11-08 05:03:35"
+    bidding_price  15
+    user_id        1
+    product_id     1
+  end
 end
