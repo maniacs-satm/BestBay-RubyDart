456f033d5a390eee9d65a5e2bd0a60bfed993ab5
xintongzhang <annabelle.cmu@gmail.com>
2012-11-29 13:07:50 -0500

    add administrator feature to manage users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,12 +19,18 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to :controller => 'products', :action => 'index'
+
+      if user.admin
+        redirect_to :controller => 'users', :action => 'index'
+      else
+        redirect_to :controller => 'products', :action => 'index'
+      end
     else
       flash[:notice] = "Invalid Email/Password combination"
       render 'new'
     end
   end
+
   # This method will sign out
   #
   # * *Handles* :
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120185449) do
+ActiveRecord::Schema.define(:version => 20121129002021) do
 
   create_table "bids", :force => true do |t|
     t.float    "bidding_price"
@@ -69,10 +69,11 @@ ActiveRecord::Schema.define(:version => 20121120185449) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
