701307e5b677f4f91a894ec81b7c12c6405d2f0b
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-11-30 12:21:07 -0500

    add count for my watchlist

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -13,7 +13,6 @@ class WatchlistsController < ApplicationController
   #
   def create
     @product = Product.find(params[:id])
-    puts Watchlist.find(:all, :conditions =>{:product_id => @product.id, :user_id => current_user.id}).size
     if Watchlist.find(:all, :conditions =>{:product_id => @product.id, :user_id => current_user.id}).size != 0
       flash[:notice] = "Product already in watchlist"
     else
